ffe1d2d4803b5430fca85c1e82a3ce742da82344
mateus lavezo <ssmokeagain@gmail.com>
2018-09-26 17:28:43 -0300

    exercicio5publicandoprojetos
    publique pelo menos 2 exercicios da materia PA

---
+++ ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intervalo 10 e 20");
+            Console.WriteLine("-----------------");
+
+            int n = 0;
+            int intervalo = 0;
+
+            int i = 1;
+            while (i <= 7)
+            {
+                Console.Write("Digite o " + i + "º numero: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if (n >= 10 && n <= 20)
+                {
+                    intervalo++;
+                }
+        
+                i++;
+            }
+            Console.WriteLine("Qtde numeros no intervalo de 10 e 20: " + intervalo);
+            Console.ReadKey();
+        }
+    }
+}
